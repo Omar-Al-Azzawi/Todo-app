d4dcca88bb74a78dc832514b9fd8aff060e141d1
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2022-03-02 20:53:50 +0200

    add style

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function TodoForm(props) {
   const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -27,6 +33,7 @@ export default function TodoForm(props) {
         value={input}
         name="text"
         onChange={handleChange}
+        ref={inputRef}
       />
       <button className="todo-button" type="submit">
         Add
