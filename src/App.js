da41b7ac316b361e8f06af893f2cb020240d391f
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2022-03-02 14:18:23 +0200

    create todo form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import TodoForm from "./Components/TodoForm";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>ToDo</h1>
+      <TodoForm />
     </div>
   );
 }
--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoForm() {
-  return <div>TodoForm</div>;
+  const [input, setInput] = useState("");
+
+  return (
+    <form className="todo-form">
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Add todo"
+        value={input}
+        name="text"
+      />
+      <button className="todo-button" type="submit">
+        Add
+      </button>
+    </form>
+  );
 }
