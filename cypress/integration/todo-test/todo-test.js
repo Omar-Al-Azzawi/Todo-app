f373e41aeff75e2ac7598081397182926b2e898a
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2022-03-03 13:50:26 +0200

    test it with cypress

--- cypress/integration/todo-test/todo-test.js
+++ cypress/integration/todo-test/todo-test.js
@@ -20,4 +20,16 @@ describe("test todo app", () => {
       .and("contain", "Learn Redux")
       .and("contain", "Learn React");
   });
+  it("test the length of the todo list", () => {
+    cy.get("[data-cy=todo-row]").should("have.length", 3);
+    cy.get("[data-cy=todo-input]").type("Learn more about Cypress");
+    cy.get("[data-cy=todo-btn]").click();
+    cy.get("[data-cy=todo-row]").should("have.length", 4);
+  });
+  it("test the complete todo", () => {
+    cy.get("[data-cy=todo-row]").should("have.class", "todo-row");
+    cy.get("[data-cy=todo-text]").first().click();
+    // test if the todo is completed or not by check the class complete!
+    cy.get("[data-cy=todo-row]").first().should("have.class", "complete");
+  });
 });
