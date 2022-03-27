2ad4e10feea0cd2d86cfd183f9424dedaea660d0
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2022-03-27 21:44:27 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import TodoList from "./Components/TodoList";
+import { MdOutlineDarkMode, MdWbSunny } from "react-icons/md";
+
+import { ThemeContext } from "./context/theme";
 
 import "./App.css";
 
 function App() {
+  const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ backgroundColor: theme.background, color: theme.color }}
+    >
       <h1>TODO</h1>
+      <button
+        className="theme-btn"
+        style={{ color: theme.color, transition: "all 1s ease" }}
+        onClick={toggleTheme}
+      >
+        {isDark ? <MdWbSunny /> : <MdOutlineDarkMode />}
+      </button>
       <TodoList />
     </div>
   );
